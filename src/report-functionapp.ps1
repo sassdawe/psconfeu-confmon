41a180522f2e7b324cae8c914fbadf275c1e62ba
David Sass <elethulyelenseg@hotmail.com>
2023-06-22 07:18:54 +0000

    function-config

--- src/report-functionapp.ps1
+++ src/report-functionapp.ps1
@@ -1,2 +1,12 @@
 Write-Output "Getting Function App List...";
-(az functionapp list | convertfrom-json | %{ $_ } | sort-object id ) | convertto-json | out-file .\report\functionapp-list.json;
+$functions = (az functionapp list | convertfrom-json | foreach-object { $_ } | sort-object id ) 
+
+$functions | convertto-json | out-file .\report\functionapp-list.json;
+
+Write-Output "Getting Function App configs...";
+
+$function_config = $functions | foreach-object {
+    az functionapp config show --ids $_.id | convertfrom-json
+}
+
+$function_config | convertto-json | out-file .\report\functionapp-list.json;

0783c47f7b5f770addd078e1610193280ace8530
David Sass <elethulyelenseg@hotmail.com>
2023-06-22 07:28:05 +0000

    function-identity

--- src/report-functionapp.ps1
+++ src/report-functionapp.ps1
@@ -1,12 +1,17 @@
 Write-Output "Getting Function App List...";
 $functions = (az functionapp list | convertfrom-json | foreach-object { $_ } | sort-object id ) 
-
 $functions | convertto-json | out-file .\report\functionapp-list.json;
 
-Write-Output "Getting Function App configs...";
 
+Write-Output "Getting Function App configs...";
 $function_config = $functions | foreach-object {
     az functionapp config show --ids $_.id | convertfrom-json
 }
-
 $function_config | convertto-json | out-file .\report\functionapp-config.json;
+
+
+Write-Output "Getting Function App identity...";
+$function_identity = $functions | foreach-object {
+    az functionapp identity show --ids $_.id | convertfrom-json
+}
+$function_identity | convertto-json | out-file .\report\functionapp-identity.json;

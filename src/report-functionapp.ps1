706954ab8d671776994c3d02bc4c51a434f9efbc
David Sass <elethulyelenseg@hotmail.com>
2023-06-22 09:05:58 +0200

    Update report-functionapp.ps1

--- src/report-functionapp.ps1
+++ src/report-functionapp.ps1
@@ -1,2 +1,2 @@
 Write-Output "Getting Function App List...";
-(az functionapp list | convertfrom-json | %{ $_ } | sort-object id ) | convertto-json | out-file .\functionapp-list.json;
+(az functionapp list | convertfrom-json | %{ $_ } | sort-object id ) | convertto-json | out-file .\report\functionapp-list.json;

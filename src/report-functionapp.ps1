c6e439efcbb64064ff98e806184f87d609cc89ab
David Sass <elethulyelenseg@hotmail.com>
2023-06-22 07:22:12 +0000

    fix(report) output

--- src/report-functionapp.ps1
+++ src/report-functionapp.ps1
@@ -9,4 +9,4 @@ $function_config = $functions | foreach-object {
     az functionapp config show --ids $_.id | convertfrom-json
 }
 
-$function_config | convertto-json | out-file .\report\functionapp-list.json;
+$function_config | convertto-json | out-file .\report\functionapp-config.json;
